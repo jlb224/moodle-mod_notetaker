d75cf785821b60275e0e726e7227d778142ec989
Jo Beaver <jobeaveris@hotmail.com>
2020-06-07 20:59:27 +0100

    Add icon

--- mod_form.php
+++ mod_form.php
@@ -22,6 +22,8 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace mod_notetaker\lib;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/course/moodleform_mod.php');
