dd48e9d399399ecb834ce383164a7e0df4284759
Jo Beaver <jobeaveris@hotmail.com>
2020-08-04 19:52:22 +0100

    Fix coding violations

--- backup/moodle2/restore_notetaker_activity_task.class.php
+++ backup/moodle2/restore_notetaker_activity_task.class.php
@@ -24,7 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Notetaker restore task
+ * Notetaker restore task that provides all the settings and steps to perform
+ * one complete restore of the activity
  */
 
 require_once($CFG->dirroot . '/mod/notetaker/backup/moodle2/restore_notetaker_stepslib.php'); // Because it exists (must).
